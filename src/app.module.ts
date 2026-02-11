c6c43e624265eaf2b090578868a6b828c071e040
God-yash12 <gannuthapa986@gmail.com>
2026-02-11 23:40:49 +0545

    feat: add throttler module for rate limiting and enhance app configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { databaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD } from '@nestjs/core/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env`, `src/.env`],
+    }),
+    ThrottlerModule.forRoot([{
+      ttl: 60000, 
+      limit: 10,
+    }]),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: databaseConfig,
+    }),
+  ],
+
+
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+     {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
